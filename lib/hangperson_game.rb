2fe2123eeb24a20e7f6450045fee20ab15ece5a3
Jorgeas <janguita@gmail.com>
2015-11-04 21:39:19 +0000

    finished hangperson, tests passed

--- lib/hangperson_game.rb
+++ lib/hangperson_game.rb
@@ -7,9 +7,44 @@ class HangpersonGame
 
   # def initialize()
   # end
+
+  attr_accessor :word, :guesses, :wrong_guesses
   
   def initialize(word)
     @word = word
+    @guesses = ''
+    @wrong_guesses = ''
+  end
+
+  def guess(letter)
+    raise ArgumentError, 'Argument is not a letter' unless !letter.nil? and letter != '' and letter =~ /^[[:alpha:]]$/
+
+    if word =~ /#{letter}/i and @guesses !~/#{letter}/i
+	@guesses += letter.downcase
+        return true
+    elsif word !~ /#{letter}/i and  @wrong_guesses !~/#{letter}/i
+        @wrong_guesses += letter.downcase
+        return true
+    end
+    return false
+  end
+
+  def word_with_guesses
+    if @guesses == ''
+      @word.gsub /[^.]/, '-'
+    else
+      @word.gsub /[^#{guesses}]/, '-'
+    end
+  end
+
+  def check_win_or_lose
+    if @wrong_guesses.length >= 7
+      :lose
+    elsif @word == self.word_with_guesses
+      :win
+    else
+      :play
+    end
   end
 
   def self.get_random_word
@@ -20,3 +55,10 @@ class HangpersonGame
   end
 
 end
+
+game = HangpersonGame.new('banana')
+
+puts game.guess('d')
+puts game.word_with_guesses
+puts game.guess('e')
+puts game.word_with_guesses
